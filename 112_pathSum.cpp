b404963addc9207de3ab79feaaac97654c1f41cb
Zheng <zzjohnchn@gmail.com>
2017-09-22 16:38:27 -0700

    delete C++

--- 112_pathSum.cpp
+++
@@ -1,8 +0,0 @@
-bool hasPathSum(TreeNode* root, int sum)
-{
-	if(root == NULL)
-		return false;
-	if(sum==root->val && root->left==NULL && root->right==NULL)
-		return true;
-	return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
-}
--- 136_singleNumber.cpp
+++
@@ -1,8 +0,0 @@
-int singleNumber(vector<int>& nums)
-{
-	int n = nums.size();
-	int res = 0;
-	for(int i=0; i<=n-1; i++)
-		res ^= nums[i];
-	return res;
-}
